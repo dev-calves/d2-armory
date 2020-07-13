8910bc76d5294b1899654179aa50fca18affed1d
dev-calves <alves.c90@outlook.com>
2020-07-13 15:29:29 -0400

    environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    production: true
+    production: true,
+    CHARACTERS_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/characters',
+    CURRENT_USER_MEMBERSHIP_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/current-user-membership',
+    ENCRYPT_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/encrypt',
+    DECRYPT_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/decrypt',
+    OAUTH_ACCESS_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/oauth/access',
+    OAUTH_DELETE_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/oauth/delete',
+    OAUTH_REFRESH_ENDPOINT: 'https://d2-armory-api.herokuapp.com/api/oauth/refresh',
+    LOCAL_STORAGE_STORAGE: 'storage',
+    LOCAL_STORAGE_DISMISS_LOGON_MESSAGE: 'dismiss-logon-message',
+    LOCAL_STORAGE_STATE: 'state'
   };
