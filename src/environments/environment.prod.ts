5aadbfcb5787d1565179905b7e1178168b000d09
dev-calves <alves.c90@outlook.com>
2021-04-29 00:11:53 -0400

    endpoint changed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
     DECRYPT_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/decrypt',
     OAUTH_ACCESS_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/oauth/access',
     OAUTH_DELETE_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/oauth/delete',
-    OAUTH_REFRESH_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/oauth/refresh',
+    OAUTH_REFRESH_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/oauth/refresh-status',
     EQUIPMENT_ENDPOINT: 'https://destiny2armory.herokuapp.com/api/equipments',
     LOCAL_STORAGE_STORAGE: 'storage',
     LOCAL_STORAGE_DISMISS_LOGON_MESSAGE: 'dismiss-logon-message',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
     DECRYPT_ENDPOINT: 'http://127.0.0.1:3000/api/decrypt',
     OAUTH_ACCESS_ENDPOINT: 'http://127.0.0.1:3000/api/oauth/access',
     OAUTH_DELETE_ENDPOINT: 'http://127.0.0.1:3000/api/oauth/delete',
-    OAUTH_REFRESH_ENDPOINT: 'http://127.0.0.1:3000/api/oauth/refresh',
+    OAUTH_REFRESH_ENDPOINT: 'http://127.0.0.1:3000/api/oauth/refresh-status',
     EQUIPMENT_ENDPOINT: 'http://127.0.0.1:3000/api/equipments',
     LOCAL_STORAGE_STORAGE: 'storage',
     LOCAL_STORAGE_DISMISS_LOGON_MESSAGE: 'dismiss-logon-message',
